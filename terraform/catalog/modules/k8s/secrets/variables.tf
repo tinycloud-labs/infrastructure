5d589deea913e2918cabb73ec36520d8968c351b
Shakir Almasaari <35549045+shakir85@users.noreply.github.com>
2026-04-10 21:25:29 -0700

    feat: add secrets tf module

---
+++ terraform/catalog/modules/k8s/secrets/variables.tf
@@ -0,0 +1,44 @@
+# tflint-ignore: terraform_unused_declarations
+variable "config_path" {
+  description = "Path to kubeconfig file relative to where this script will run"
+  type        = string
+}
+
+# tflint-ignore: terraform_unused_declarations
+variable "config_context" {
+  description = "The name of the kubeconfig context to use"
+  type        = string
+}
+
+variable "secret_name" {
+  description = "K8s Secret resource name"
+  type        = string
+}
+
+variable "kube_namespace" {
+  description = "K8s namespace where the resource should be created"
+  type        = string
+}
+
+variable "labels" {
+  description = "Optional resource labels"
+  type        = map(string)
+  default     = {}
+}
+
+variable "annotations" {
+  description = "Optional resource annotations"
+  type        = map(string)
+  default     = {}
+}
+
+variable "secret_data" {
+  description = "Content of the K8s secret as a key:value pair"
+  type        = map(string)
+}
+
+variable "type" {
+  description = "Type of the K8s Secret. Default to Opauqe"
+  type        = string
+  default     = "Opaue"
+}
