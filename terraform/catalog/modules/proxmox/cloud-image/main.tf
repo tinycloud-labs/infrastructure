1a1cf82b070e38c968ce0616bfaa2353cf06d609
Shakir Almasaari <35549045+shakir85@users.noreply.github.com>
2026-09-20 17:58:37 -0700

    bump module version

--- terraform/catalog/modules/proxmox/cloud-image/main.tf
+++ terraform/catalog/modules/proxmox/cloud-image/main.tf
@@ -1,5 +1,5 @@
 module "cloud-image" {
-  source            = "git::https://github.com/shakir85/terraform_modules.git//proxmox/cloud-img-download?ref=0.3.11"
+  source            = "git::https://github.com/shakir85/terraform_modules.git//proxmox/cloud-img-download?ref=1.0.0"
   proxmox_node_name = var.node_name
   cloud_image_url   = var.cloud_image_url
   storage_pool      = var.datastore_id
